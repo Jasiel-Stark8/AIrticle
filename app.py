e14ed414f28bd799f8f9f1825c280d5e5e447582
== <==>
2023-10-17 18:44:19 +0000

    Add app configurations

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from models import user
 from api.v1 import articles
 from api.v1 import auth
 from api.v1 import autosave
+from config import DevelopmentConfig, Config, OpenaiConfig
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(DevelopmentConfig)
+app.config.from_object(Config)
+app.config.from_object(OpenaiConfig)
 
 @app.route('/')
 def home():
-    return "Serving Page"
+    return render_template('landig.html')
 
 
 if __name__ == '__main__':
