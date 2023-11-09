b4d0d7a0e18c61c4f2edd2e85aaac49e5aaa897d
== <==>
2023-11-09 23:05:45 +0000

    text function

--- models/article.py
+++ models/article.py
@@ -8,7 +8,7 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text(), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     user = db.relationship('User', back_populates='articles')
